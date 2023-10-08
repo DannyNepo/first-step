117bd62222c9994eadd47c1ecc394dbc3fba7a2e
DaniilNepomnyashchikh <daniil.nepomnyaschih@game-garden.com>
2023-10-08 20:10:35 +0700

    initial

---
+++ FirstStep/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FirstStep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.ReadKey();
+
+            Console.WriteLine("Helo, ");
+            Console.WriteLine("world ");
+            Console.WriteLine("!!!");
+            Console.ReadKey();
+        }
+    }
+}
